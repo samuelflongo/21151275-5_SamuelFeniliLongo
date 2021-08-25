5ea1bb690356b94bbf4d9a17579e8ad6d8c6cf43
Samuel Fenili Longo <87680499+samuelflongo@users.noreply.github.com>
2021-08-25 19:31:49 -0300

    Add files via upload

--- dados/produtos.php
+++ dados/produtos.php
@@ -5,57 +5,66 @@ $produto = array (
     "nome" => "Doce de Abacaxi",
     "descricao" => "Receita de origem portuguesa, esse doce é leve e saborososo, ideal para os dias quentes",
     "ingredientes" => "Abacaxi, Água, Açúcar Demerada e Cravo da Ídia",
-    "link" => "./images/doce_abacaxi.jpg"
+    "link" => "./images/doce_abacaxi.jpg",
+    "preco" => "R$ 12,00"
     ),
 
     array (
     "nome" => "Doce de Amora",
     "descricao" => "Receita originária da Bélgica, esse doce traz uma sensação de acolhimento e ajuda a reunir a família",
     "ingredientes" => "Amoras, Água, Açúcar Demerada",
-    "link" => "./images/doce_amora.jpg"
+    "link" => "./images/doce_amora.jpg",
+    "preco" => "R$ 14,00"
     ),
 
     array (
     "nome" => "Doce de Caju",
     "descricao" => "Receita nordestina, esse doce propicia climas festivos, por isso o ideal é servir em dias festivos",
     "ingredientes" => "Caju, Água e Açúcar Demerada",
-    "link" => "./images/doce_caju.jpg"
+    "link" => "./images/doce_caju.jpg",
+    "preco" => "R$ 16,00"
     ),
 
     array (
     "nome" => "Doce de Figo",
     "descricao" => "Receita oriundo do sul do Brasil, esse doce casa muito bem com comidas quentes, sendo ideal para os dias mais frios",
     "ingredientes" => "Figo, Água, Açúcar Demerada e Cravo da Índia",
-    "link" => "./images/doce_figo.jpg"
+    "link" => "./images/doce_figo.jpg",
+    "preco" => "R$ 12,00"
     ),
 
     array (
     "nome" => "Doce de Leite",
     "descricao" => "Receita vinda da argentina, esse doce é muito utilizado para recheios de bolos, mas também casa bem um gosto café",
     "ingredientes" => "Leite Condensado, Açúcar Mascavo e Manteiga",
-    "link" => "./images/doce_leite.jpg"
+    "link" => "./images/doce_leite.jpg",
+    "preco" => "R$ 15,00"
     ),
 
     array (
     "nome" => "Doce de Morango",
     "descricao" => "Receita originária dos Andes, esse doce é muito bem vindo com um chá de frutas",
     "ingredientes" => "Morangos, Água e Açúcar Demerara",
-    "link" => "./images/doce_morango.jpg"
+    "link" => "./images/doce_morango.jpg",
+    "preco" => "R$ 14,00"
     ),
 
     array (
         "nome" => "Doce de Mamão",
         "descricao" => "Receita originária da Inglaterra, muito consumido com torrados e chás",
         "ingredientes" => "Mamão verde, Água e Açúcar Demerara",
-        "link" => "./images/doce_mamao.jpg"
+        "link" => "./images/doce_mamao.jpg",
+        "preco" => "R$ 12,00"
     ),
     
     array (
         "nome" => "Doce de Coco",
         "descricao" => "Receita trazida do Havai, onde culturalmente se consome junto com bebidas destiladas",
         "ingredientes" => "Coco, Água e Açúcar Mascavo",
-        "link" => "./images/doce_coco.jpg"
+        "link" => "./images/doce_coco.jpg",
+        "preco" => "R$ 13,00"
     )
+
 );
 
 
